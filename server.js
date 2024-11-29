3452fbe88ed161c9ed8b2f556316e4359be1f38e
johnsonomonu <johnsonomonu144@gmail.com>
2024-11-29 21:09:52 +0100

    commit

--- server.js
+++ server.js
@@ -1,101 +1,105 @@
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
-const env = require("dotenv").config();
-console.log("SESSION_SECRET:", process.env.SESSION_SECRET); // For debugging
-
-const app = express();
-const static = require("./routes/static");
-const baseController = require("./controllers/baseController");
-const inventoryRoute = require("./routes/inventoryRoute");
-const accountRoute = require("./routes/accountRoute");
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const errorRoute = require("./routes/errorRoute");
-const utilities = require("./utilities/");
-const session = require("express-session");
-const pool = require('./database/');
-const flash = require('connect-flash');
-const expressMessages = require('express-messages');
+const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
- ************************/
-
-
+ * ************************/
+console.log("Using session secret:", process.env.SESSION_SECRET);
 
 app.use(session({
   store: new (require('connect-pg-simple')(session))({
     createTableIfMissing: true,
     pool,
   }),
-  secret: process.env.SESSION_SECRET, // This should pull the secret from .env
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   name: 'sessionId',
 }))
 
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
-// Flash messages middleware
-app.use(flash());
-app.use((req, res, next) => {
-  res.locals.messages = expressMessages(req, res);
-  next();
-});
-
-// For parsing application/json and application/x-www-form-urlencoded
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// For parsing application/x-www-form-urlencoded
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs");
-app.use(expressLayouts);
-app.set("layout", "./layouts/layout");
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static);
+app.use(static)
 
 // Index route
-app.get("/", utilities.handleErrors(baseController.buildHome));
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
-app.use("/inv", inventoryRoute);
+app.use("/inv", inventoryRoute)
 
 // Account routes
-app.use("/account", accountRoute);
+app.use("/account", accountRoute)
 
 // Intentional error route
 app.use("/error", errorRoute);
 
 // File Not Found Route - must be last route in list
-app.use((req, res, next) => {
-  next({ status: 404, message: 'Sorry, we appear to have lost that page.' });
-});
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 /* ***********************
- * Express Error Handler
- *************************/
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
-  let message = err.status === 404 ? err.message : '&#x1F4A5 Oh no! There was a crash. This is a 500 error. &#x1F4A5 We will investigate. &#x1F46E';
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! This is a 500 error.'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
     message,
     nav
-  });
-});
+  })
+})
 
 /* ***********************
  * Local Server Information
+ * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000;
-const host = process.env.HOST || 'localhost';
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`App listening on http://${host}:${port}`);
-});
+  console.log(`app listening on ${host}:${port}`)
+})
